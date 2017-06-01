7b7dbc6f6c645ee6a7c5c8cef059f5e981a8cfcf
Евгений <@HOME-PC>
2017-06-01 21:11:20 +1000

    2541

--- PathToSky/MathCore/cmdDeleteLine.cs
+++ PathToSky/MathCore/cmdDeleteLine.cs
@@ -25,21 +25,24 @@ namespace MathCore
                 if (toAnalyse.Substring(i * data.WidthGameField, lenghtTail).Split('0').Length-1 == lenghtTail)// Узнаем сколько нулей в строчке если равно длине то удаляем
                  {
                         toAnalyse = toAnalyse.Remove(i * data.WidthGameField, lenghtTail);
+                       //Корректируем пары
+                       int corrPosition = i * data.WidthGameField + data.WidthGameField;// Позиция после которой нужно корректировать пары
+                       for (int j = 0; j < data.CurrentPairList.Count; j++)
+                        {
+                            if (data.CurrentPairList[j].NumFirst.Position > corrPosition) data.CurrentPairList[j].NumFirst.Position  -= data.WidthGameField;
+                            if (data.CurrentPairList[j].NumSecond.Position > corrPosition) data.CurrentPairList[j].NumFirst.Position -= data.WidthGameField;
+                        }
                         OUT.Add(i); //Номер удаленной строки для тех кому это надо
                         lineCount--; //количество линий уменьшилось
                         i--;//так как строку удалили интератор уменьшаем
                  }
             }
+
+            //Обновялем поле
             data.GameField.Clear();
             data.GameField.Append(toAnalyse);
-            if (data.GameField.Length > 0)
-            {
-                data.RefreshCuplesData();
-            }
-            else
-            {
-                data.GameFinish(true);
-            }
+
+            if (data.GameField.Length == 0) data.GameFinish(true); //Если линий нет то конец игры. 
 
             return OUT;
         }
