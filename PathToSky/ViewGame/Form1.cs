5d8dad6bbfdf9f5599395853fe75da96463fc8fc
Евгений <@HOME-PC>
2017-06-05 23:36:17 +1000

    asdasd

--- PathToSky/ViewGame/Form1.cs
+++ PathToSky/ViewGame/Form1.cs
@@ -13,15 +13,12 @@ namespace ViewGame
 {
     public partial class Form1 : Form
     {
+        Core c;
         public Form1()
         {
             InitializeComponent();
-            Core c =  new Core();
-            c.Run(new cmdGenerateField("020000300" + "020000300" + "020000000"));
-            c.Run(new cmdDeletePair(1, 10));
-            DrawConsoleReport(c);
-            c.Undo();
-            DrawConsoleReport(c);
+            c =  new Core();
+            c.Run(new cmdGenerateField("123456789" + "123456789"));
         }
         public void DrawConsoleReport(Core c)
         {
@@ -29,6 +26,15 @@ namespace ViewGame
             for (int i = 0; i < s.Count; i++)
             {
                 Console.WriteLine(s[i]);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            for (int i = 0; i < c.Field.Length; i++)
+            {           
+                    g.DrawImage(ResourceManager.GetBitMap(c.Field[i]), new Rectangle( (i%9) * 20, (i / 9) * 20, 20, 20));
             }
         }
     }
---
+++ PathToSky/ViewGame/ResourceManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ViewGame
+{
+    public class ResourceManager
+    {
+        public static Bitmap GetBitMap(char c)
+        {
+            switch (c)
+            {
+                case '0':
+                    return ViewGame.Properties.Resources.Num_00;
+                case '1':
+                    return ViewGame.Properties.Resources.Num_01;
+                case '2':
+                    return ViewGame.Properties.Resources.Num_02;
+                case '3':
+                    return ViewGame.Properties.Resources.Num_03;
+                case '4':
+                    return ViewGame.Properties.Resources.Num_04;
+                case '5':
+                    return ViewGame.Properties.Resources.Num_05;
+                case '6':
+                    return ViewGame.Properties.Resources.Num_06;
+                case '7':
+                    return ViewGame.Properties.Resources.Num_07;
+                case '8':
+                    return ViewGame.Properties.Resources.Num_08;
+                case '9':
+                    return ViewGame.Properties.Resources.Num_09;
+            }
+            return null;
+        }
+    }
+
+
+}
